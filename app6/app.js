0675893f3c575fc85bca6128417870d0971f61f5
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-07-06 13:18:46 +0700

    update

--- app6/app.js
+++
@@ -1,8 +0,0 @@
-
-import express from 'express';
-import bodyParser from 'body-parser';
-
-export const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-
--- app6/constants.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 3000;
-export const SUCCESS = "success";
-export const FAILED = "failed";
