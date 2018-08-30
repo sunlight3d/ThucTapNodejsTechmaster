90f12cb29354ab5894f9e3d669cf73fef4665ddb
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-08-30 20:27:28 +0700

    update

---
+++ uploading/01-aaa/code/client-react/src/index.js
@@ -0,0 +1,12 @@
+/*
+npm install --save mdbreact
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
